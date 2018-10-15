c1e840fa9f7053fc52285878148e48ee3b5418f7
cimryan <cimryan@hotmail.com>
2018-10-14 20:39:12 -0700

    Make calculations of backng file size in terms of free 1k blocks

--- windows_archive/create-backingfiles.sh
+++ windows_archive/create-backingfiles.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -eu
 
-SPACE_TO_USE="$1"
-CAM_PERCENT="$2"
-BACKINGFILES_MOUNTPOINT="$3"
+CAM_PERCENT="$1"
+BACKINGFILES_MOUNTPOINT="$2"
 
 G_MASS_STORAGE_CONF_FILE_NAME=/etc/modprobe.d/g_mass_storage.conf
 
@@ -12,7 +11,7 @@ function add_drive () {
   local size="$3"
 
   local filename="$4"
-  fallocate -l "$size" "$filename"
+  fallocate -l "$size"K "$filename"
   mkfs.vfat "$filename" -F 32 -n "$label"
 
   local mountpoint=/mnt/"$name"
@@ -21,14 +20,15 @@ function add_drive () {
   echo "$filename $mountpoint vfat noauto,users,umask=000 0 0" >> /etc/fstab
 }
 
-CAM_DISK_SIZE="$(( $SPACE_TO_USE * $CAM_PERCENT / 100 ))"
+FREE_1K_BLOCKS="$(df --output=avail --block-size=1K /backingfiles/ | tail -n 1)"
+
+CAM_DISK_SIZE="$(( $FREE_1K_BLOCKS * $CAM_PERCENT / 100 ))"
 CAM_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/cam_disk.bin"
 add_drive "cam" "CAM" "$CAM_DISK_SIZE" "$CAM_DISK_FILE_NAME"
 
 if [ "$CAM_PERCENT" -lt 100 ]
 then
-  MUSIC_PERCENT="$(( 100 - $CAM_PERCENT ))"
-  MUSIC_DISK_SIZE="$(( $SPACE_TO_USE * $MUSIC_PERCENT / 100 ))"
+  MUSIC_DISK_SIZE="$(df --output=avail --block-size=1K /backingfiles/ | tail -n 1)"
   MUSIC_DISK_FILE_NAME="$BACKINGFILES_MOUNTPOINT/music_disk.bin"
   add_drive "music" "MUSIC" "$MUSIC_DISK_SIZE" "$MUSIC_DISK_FILE_NAME"
   echo "options g_mass_storage file=$CAM_DISK_FILE_NAME,$MUSIC_DISK_FILE_NAME removable=1,1 ro=0,0 stall=0 iSerialNumber=123456" > G_MASS_STORAGE_CONF_FILE_NAME
