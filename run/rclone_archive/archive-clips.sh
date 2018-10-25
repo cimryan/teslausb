9bb8f1e0463cf5886e17c921ee6d6644b4051a22
Ray Tanaka <ray@poynt.co>
2018-10-25 15:18:06 -0700

    adding support and documentation for using rclone

---
+++ run/rclone_archive/archive-clips.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+
+log "Moving clips to rclone archive..."
+
+source /root/.teslaCamRcloneConfig
+
+NUM_FILES_MOVED=0
+
+for file_name in "$CAM_MOUNT"/TeslaCam/saved*; do
+  [ -e "$file_name" ] || continue
+  log "Moving $file_name ..."
+  rclone move "$file_name" "$drive:$path" >> "$LOG_FILE" 2>&1 || echo ""
+  log "Moved $file_name."
+  NUM_FILES_MOVED=$((NUM_FILES_MOVED + 1))
+done
+log "Moved $NUM_FILES_MOVED file(s)."
+
+/root/bin/send-pushover "$NUM_FILES_MOVED"
+
+log "Finished moving clips to rclone archive"
