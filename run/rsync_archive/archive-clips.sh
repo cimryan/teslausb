c3554cac89f96c31db0f4a122541a9ab80a85611
Gocnak <Gocnak@users.noreply.github.com>
2018-10-29 00:23:15 -0400

    Use compression (-z) with rsync
    This was dropped during the great merge of '18

--- run/rsync_archive/archive-clips.sh
+++ run/rsync_archive/archive-clips.sh
@@ -4,7 +4,7 @@ log "Archiving through rsync..."
 
 source /root/.teslaCamRsyncConfig
 
-num_files_moved=$(rsync -auvh --stats --log-file=/tmp/archive-rsync-cmd.log /mnt/cam/TeslaCam/saved* $user@$server:$path | awk '/files transferred/{print $NF}')
+num_files_moved=$(rsync -auzvh --stats --log-file=/tmp/archive-rsync-cmd.log /mnt/cam/TeslaCam/saved* $user@$server:$path | awk '/files transferred/{print $NF}')
 
 /root/bin/send-pushover "$num_files_moved"
 
