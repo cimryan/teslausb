d908f7caed75b23ad8ebe7a885dff53a191f23f8
Ray Tanaka <ray@poynt.co>
2018-10-31 00:00:05 -0700

    addig rclone verification step

--- run/rclone_archive/verify-archive-configuration.sh
+++ run/rclone_archive/verify-archive-configuration.sh
@@ -1,1 +1,12 @@
 #!/bin/bash -eu
+
+function verify_configuration () {
+    echo "Verifying rlcone configuration..."
+    if ! rclone lsd "$RCLONE_DRIVE": | grep -q "$RCLONE_PATH"
+    then
+        echo "STOP: Could not find the $RCLONE_DRIVE:$RCLONE_PATH"
+        exit 1
+    fi
+}
+
+verify_configuration
