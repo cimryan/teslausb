34690ca9d408d66cb7adb48245204bc09c9954c1
Ray Tanaka <ray@poynt.co>
2018-10-31 00:36:27 -0700

    add logic to move config file into /mutable as part of configuring the archive

--- run/rclone_archive/configure-archive.sh
+++ run/rclone_archive/configure-archive.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -eu
 
 function configure_archive () {
-  echo "Configuring the archive for Rclone..."
+  echo "Configuring rclone archive..."
   
   echo "drive=$RCLONE_DRIVE" > /root/.teslaCamRcloneConfig
   echo "path=$RCLONE_PATH" >> /root/.teslaCamRcloneConfig
+
+  if [ ! -L "/root/.config/rclone" ] && [ -e "/root/.config/rclone" ]
+  then
+    echo "Moving rclone configs in /mutable"
+    mv /root/.config/rclone /mutable/configs
+    ln -s /mutable/configs/rclone /root/.config/rclone
+  fi
 }
 
 configure_archive
