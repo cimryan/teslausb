fd4d161f1a28227dd96f80175248bef1104da138
Ray Tanaka <ray@poynt.co>
2018-10-31 00:37:11 -0700

    also check for configuration file as part of verification step

--- run/rclone_archive/verify-archive-configuration.sh
+++ run/rclone_archive/verify-archive-configuration.sh
@@ -2,6 +2,12 @@
 
 function verify_configuration () {
     echo "Verifying rlcone configuration..."
+    if ! [ -e "/root/.config/rclone/rclone.conf" ]
+    then
+        echo "STOP: rclone config was not found. did you configure rclone correctly?"
+        exit 1
+    fi
+
     if ! rclone lsd "$RCLONE_DRIVE": | grep -q "$RCLONE_PATH"
     then
         echo "STOP: Could not find the $RCLONE_DRIVE:$RCLONE_PATH"
