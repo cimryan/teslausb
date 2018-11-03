fdbc6595278bfaf437c8814f41fac3f32a8ad4b6
Cisien <ccurwick@exoronet.net>
2018-11-03 16:30:46 -0700

    switch to using hping3 for connectivity testing/ip resolution

--- run/lookup-ip-address.sh
+++ run/lookup-ip-address.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -eu
 
-echo "$(ping -c 1 -w 1 $1 2>/dev/null | head -n 1 | grep -o -e "(\([[:digit:]]\{1,3\}\.\)\{3\}[[:digit:]]\{1,3\})" | tr -d '()')"
+echo "$(hping3 -c 1 -S -p 445 -n $1 2>/dev/null | head -n 1 | grep -o -e "\W\([[:digit:]]\{1,3\}\.\)\{3\}[[:digit:]]\{1,3\})" | tr -d ' )')"
